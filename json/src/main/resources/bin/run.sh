d994bea0a777da05ddf0167d2af2c80f5a9158c8
dixson <dixson.huie@gmail.com>
2020-09-03 07:46:35 -0400

    rest_code_4

--- json/src/main/resources/bin/run.sh
+++ json/src/main/resources/bin/run.sh
@@ -6,4 +6,4 @@ PROJ_DIR="/home/ubuntu/stockprediction"
 export GS_LOOKUP_LOCATORS="172.31.0.64:4174"
 export GS_LOOKUP_GROUPS="xap-15.2.0"
 
-java -jar $PROJ_DIR/restserver/target/jsonexample-1.0-SNAPSHOT-jar-with-dependencies.jar
+java -jar $PROJ_DIR/json/target/json-1.0-SNAPSHOT-jar-with-dependencies.jar
---
+++ v15.2/bin/mygsui.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+source /home/ubuntu/stockprediction/v15.2/bin/setExampleEnv.sh
+
+$GS_HOME/bin/gs-ui.sh
