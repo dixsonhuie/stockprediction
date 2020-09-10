71a5324254af8fef92b13218b5a31d98f6178eaa
dixson <dixson.huie@gmail.com>
2020-09-10 17:35:32 -0400

    rest_client_code_1

--- feeder/src/main/resources/feeder.sh
+++ feeder/src/main/resources/feeder.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-PROJ_DIR="/home/ubuntu/proj/stockprediction"
+PROJ_DIR="/home/ubuntu/stockprediction"
 
-java -jar $PROJ_DIR/feeder/target/feeder-1.0-SNAPSHOT-jar-with-dependencies.jar
+java -jar $PROJ_DIR/feeder/target/feeder-1.0-SNAPSHOT-jar-with-dependencies.jar my-topic $PROJ_DIR/data.csv
+
